fcba3ff854f0df614f930e7e35a5f5ec9791ed65
Verity Hill <verity.hill@yale.edu>
2026-04-24 16:37:56 +0200

    add tree component modules

--- beastgenpy/scripts/error_checks.py
+++ beastgenpy/scripts/error_checks.py
@@ -9,3 +9,10 @@ def check_gp_cutoff(config):
     #if skygrid, check gridpoints and cutoff are present
 
     return
+
+
+def check_tree_dict(config):
+
+    #if fixed/empirical check tree files are there
+
+    return
